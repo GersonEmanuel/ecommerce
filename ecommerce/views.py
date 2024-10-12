bbcacc462e07ab1c194c3939cc3bb6463235e659
Gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-10-12 15:52:02 -0300

    methods added contact_page, about_page and home_page

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -2,4 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'index.html')
+    context={'title': 'pricipal page', 'content':'principal'}
+    return render(request, 'index.html', context)
+
+def about_page(request):
+    context = {'title':'about page', 'content':
+               'welcome to about page'}
+    return render(request, 'about_page.html', context)
+
+def contact_page(request):
+    context = {'title': 'contact page', 'content':'Welcome contact page'}
+    return render(request, 'contact_page.html', context)

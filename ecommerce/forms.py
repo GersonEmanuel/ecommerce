36258ee02838680359b2f1ba8834924cf3d553be
Gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-10-13 13:53:32 -0300

    widget in forms

--- ecommerce/forms.py
+++ ecommerce/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    fullname = forms.CharField()
-    email = forms.EmailField()
-    content = forms.CharField()
+    fullname = forms.CharField(widget = forms.TextInput(attrs={'class':'form_control', 'placeholder': 'Tell us your name'}))
+    email = forms.EmailField(widget= forms.EmailInput(attrs='class': 'form_control', 'placeholder': 'input your email'))
+    content = forms.CharField(widget= forms.Textarea(attrs= 'class': 'form_control', 'placeholder': 'send us a message'))

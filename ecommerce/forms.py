f20d359660b0620bd4250b909c9770f8afee1176
Gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-10-13 13:57:55 -0300

    error exceptions in forms

--- ecommerce/forms.py
+++ ecommerce/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    fullname = forms.CharField(widget = forms.TextInput(attrs={'class':'form_control', 'placeholder': 'Tell us your name'}))
-    email = forms.EmailField(widget= forms.EmailInput(attrs='class': 'form_control', 'placeholder': 'input your email'))
-    content = forms.CharField(widget= forms.Textarea(attrs= 'class': 'form_control', 'placeholder': 'send us a message'))
+    fullname = forms.CharField(error_messages= {'required': 'Your name is required ' },widget = forms.TextInput(attrs={'class':'form_control', 'placeholder': 'Tell us your name'}))
+    email = forms.EmailField(error_messages={'invalid', 'input a valid email'},widget= forms.EmailInput(attrs={'class': 'form_control', 'placeholder': 'input your email'}))
+    content = forms.CharField(error_messages={'required': 'its required a message '},widget= forms.Textarea(attrs= {'class': 'form_control', 'placeholder': 'send us a message'}))

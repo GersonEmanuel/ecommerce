4393e1c5a6eccc999c1d790b89966104b7a952fa
Gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-10-13 17:23:24 -0300

    new classes Login and register in forms

--- ecommerce/forms.py
+++ ecommerce/forms.py
@@ -1,5 +1,7 @@
 from django import forms
+from django.contrib.auth import get_user_model
 
+user = get_user_model()
 class ContactForm(forms.Form):
     fullname = forms.CharField(error_messages= {'required': 'Your name is required ' },widget = forms.TextInput(attrs={'class':'form_control', 'placeholder': 'Tell us your name'}))
     email = forms.EmailField(error_messages={'invalid': 'input a valid email'},widget= forms.EmailInput(attrs={'class': 'form_control', 'placeholder': 'input your email'}))
@@ -8,5 +10,16 @@ class ContactForm(forms.Form):
     def clean_email(self):
         email = self.cleaned_data.get("email")
         if not "gmail.com" in email:
-            raise forms.ValidationError("O Email deve ser do gmail.com")
+            raise forms.ValidationError("The email should be from gmail")
         return email
+    
+class LoginForms(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+class RegisterForm(forms.Form):
+    username = forms.CharField()
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    password2 = form.forms.CharField(label = 'confirm password', widget= forms.PasswordInput)
+

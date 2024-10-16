c3751fe8d7ba58d6977f8dc9e4b96b3740e1c9c7
Gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-10-16 20:42:11 -0300

    set path for media and statics and import the path in views

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from .views import *
 
@@ -8,3 +10,7 @@ urlpatterns = [
     path('login/', login_page, name='login'),
     path('register/', register_page, name='register')
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root = settings.MEDIA.ROOT)

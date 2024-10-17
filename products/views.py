95cb45e2b4ced07108e011f053ab532d8fa805d4
Gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-10-16 22:02:22 -0300

    List view in cbv and fbv

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Product
 
 # Create your views here.
+
+class ProductListView(ListView):
+    queryset = Product.objects.all()
+
+#def product_list_view(request):
+#   queryset = Product.objects.all()
+#   context = {'qs': queryset}
+#   return render(request, products/jfj.html, context)

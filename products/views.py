a8ca6f08d63f4f0e8bb6f834135539ebdb4adac2
Gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-10-16 22:33:11 -0300

    added detail view

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from typing import Any
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Product
 
 # Create your views here.
@@ -16,3 +16,19 @@ class ProductListView(ListView):
 #   queryset = Product.objects.all()
 #   context = {'qs': queryset}
 #   return render(request, products/jfj.html, context)
+
+class ProductDetailView(DetailView):
+    queryset = Product.objects.all()
+    template_name = 'products/detail.html'
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        return super().get_context_data(**kwargs)
+    
+
+#Function Based View
+#def product_detail_view(request):
+#   queryset = Product.objects.all()
+#   context = {
+#      'object_list': queryset
+# }
+#return render(request, "products/detail.html", context)

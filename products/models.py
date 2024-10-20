48a37ee04f99afe9a102a298d2a144ed97b555e3
Gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-10-20 15:27:01 -0300

    new fields in products

--- products/models.py
+++ products/models.py
@@ -31,6 +31,8 @@ class Product(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits = 20, default=10)
     image = models.ImageField(upload_to= 'products/', null=True, blank=True)
     objects = ProductManager()
+    featured = models.BooleanField(default=False)
+    active = models.BooleanField(default = True)
  
     def __str__(self):
         return self.title

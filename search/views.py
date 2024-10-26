8a036aa977e49c984795fc53a093c34a2968d5b3
Gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-10-26 17:48:47 -0300

    search view modified

--- search/views.py
+++ search/views.py
@@ -6,5 +6,10 @@ class SearchProductView(ListView):
     template_name = "products/list.html"
     def get_queryset(self, *args, **kargs):
         request = self.request
-        return Product.objects.all()
-
+        result = request.GET
+        print('Resultado:', result)
+        query = result.get('q', None) # result['q']
+        print('Consulta', query)
+        if query is not None:
+            return Product.objects.filter(title__iexact = query)
+        return Product.objects.featured()

2e2e2cba3792bc97d2569233c20e2a23a25df919
Gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-10-26 17:42:26 -0300

    new app search

--- mysite/urls.py
+++ mysite/urls.py
@@ -24,7 +24,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ecommerce.urls')),
-    path('products/', include('products.urls', namespace='products'))
+    path('products/', include('products.urls', namespace='products')),
+    path('search/', include('search.urls', namespace='search'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

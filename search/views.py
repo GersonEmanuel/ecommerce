5de2a248112d2db979acfd0d4baf3fd6a0cab9fd
Gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-10-26 19:00:44 -0300

    new method in SearchProductView

--- search/views.py
+++ search/views.py
@@ -1,9 +1,15 @@
+from typing import Any
 from django.shortcuts import render
 from django.views.generic import ListView
 from products.models import Product
 
 class SearchProductView(ListView):
-    template_name = "products/list.html"
+    template_name = "search/view.html"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        return super().get_context_data(**kwargs)
+    
+    
     def get_queryset(self, *args, **kargs):
         request = self.request
         result = request.GET

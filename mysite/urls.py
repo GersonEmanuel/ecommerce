d57a445f6639e9b167b96c524ead12c59b514225
Gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-10-26 21:55:25 -0300

    new path and home.html for cart

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from carts.views import cart_home
 
 
 
@@ -25,7 +26,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ecommerce.urls')),
     path('products/', include('products.urls', namespace='products')),
-    path('search/', include('search.urls', namespace='search'))
+    path('search/', include('search.urls', namespace='search')),
+    path('cart/', cart_home, name='cart')
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

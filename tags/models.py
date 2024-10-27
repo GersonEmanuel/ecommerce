b3d8d3bce918d2500e0e80f72ee49b6b8949e004
Gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-10-26 21:26:29 -0300

    tags inclued

--- tags/models.py
+++ tags/models.py
@@ -5,7 +5,7 @@ from django.db.models.signals import pre_save
 
 class Tag(models.Model):
     title = models.CharField(max_length = 120)
-    slug = models.SlugField()
+    slug = models.SlugField(blank=True)
     timestamp = models.DateTimeField(auto_now_add = True)
     active = models.BooleanField(default = True)
     products = models.ManyToManyField(Product, blank = True)

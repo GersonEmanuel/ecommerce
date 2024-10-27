282ffa427d7d0c3c3574e2d6c9f8e92970cc9c4a
Gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-10-27 17:39:39 -0300

    new change in cart home, yes again

--- carts/views.py
+++ carts/views.py
@@ -4,10 +4,4 @@ from .models import Cart
 # Create your views here.
 def cart_home(request):
     cart_obj, new_obj = Cart.objects.new_or_get(request)
-    products = cart_obj.products.all()
-    total = 0
-    for product in products:
-        total += product.price
-    cart_obj.total = total
-    cart_obj.save()
     return render(request, 'carts/home.html', {})

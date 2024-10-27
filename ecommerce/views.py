225c0e40a553dca57e26297a5bb636c0b4b70196
Gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-10-27 12:33:54 -0300

    troublews on register

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, get_user_model, logout
 from .forms import *
 
@@ -32,7 +33,7 @@ def login_page(request):
         user = authenticate(request, username=username, password=password) 
         if user is not None:
             login(request, user)
-            return redirect('')
+            return redirect('home')
     return render(request, 'auth/login.html', context)
 
 def logout_page(request):
@@ -50,9 +51,9 @@ def register_page(request):
               }
     if form.is_valid():
         print(form.cleaned_data)
-        username = form.cleaned_data.get("username")
-        email = form.cleaned_data.get("email")
-        password = form.cleaned_data.get("password")
+        username = form.clean_username()
+        email = form.clean_email()
+        password = form.clean()
         new_user = User.objects.create_user(username, email, password)
         print(new_user)
     return render(request, "auth/register.html", context)

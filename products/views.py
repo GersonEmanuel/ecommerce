dbd3a8c8bc48fd91704f627333f36d6ea6c36d81
Gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-10-27 18:00:14 -0300

    get context of the cart

--- products/views.py
+++ products/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.views.generic import ListView, DetailView
 from .models import *
+from carts.models import Cart
 
 # Create your views here.
 
@@ -60,6 +61,13 @@ class ProductDetailView(DetailView):
 class ProductDetailSlugView(DetailView):
     queryset = Product.objects.all()
     template_name = 'productst/detail.html'
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super(ProductDetailSlugView, self). get_context_data(self, **kwargs)
+        cart_obj, new_obj = Cart.objects.new_or_get(self.request)
+        context['cart'] = cart_obj
+        return context
+        
 
     def get_object(self, *args, **kwargs):
         slug = self.kwargs.get('slug')

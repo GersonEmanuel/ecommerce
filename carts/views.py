6c4536e02d365383471fa668f8a430a787ac04e0
Gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-10-26 22:19:41 -0300

    session

--- carts/views.py
+++ carts/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def cart_home(request):
-    return render(request, 'carts/home.html', {})
+    request.session['cart_id'] = 123
+    request.session['user'] = request.user.username
+    return render(request, 'home.html', {})

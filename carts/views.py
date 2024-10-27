f16692cbc0ce482b29d976a099eda88f2713c21b
Gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-10-27 17:20:04 -0300

    calculating total price in cart

--- carts/views.py
+++ carts/views.py
@@ -3,11 +3,11 @@ from .models import Cart
 
 # Create your views here.
 def cart_home(request):
-    cart_obj = Cart.objects.new_or_get(request)
-    cart_id = request.session.get("cart_id", None)
-    if cart_id is None:
-        print('create new cart')
-        request.session['cart_id'] = 12
-    else:
-        print("Cart ID exists")
-    return render(request, "home.html", {} )
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    products = cart_obj.products.all()
+    total = 0
+    for product in products:
+        total += product.price
+    cart_obj.total = total
+    cart_obj.save()
+    return render(request, 'carts/home.html', {})

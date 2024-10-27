81d35aba0300691ae950166a70eebe02a4b2eda7
Gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-10-27 10:45:10 -0300

    new model Cartmanager

--- carts/models.py
+++ carts/models.py
@@ -7,6 +7,24 @@ from django.db import models
 from products.models import Product
 
 User = settings.AUTH_USER_MODEL
+
+class CartManager(models.Manager):
+    def new_or_get(self, request):
+        cart_id = request.session.get('cart_id', None)
+        qs = self.get_queryset().filter(id= cart_id)
+        if qs.count() == 1:
+            new_obj = False
+            cart_obj = qs.first()
+            if request.user.is_authenticate and cart_obj.user is None:
+                cart_obj.user = request.user
+                cart_obj.save()
+            else:
+                cart_obj = Cart.objects.new(user = request.user)
+                new_obj = True
+                request.session['cart_id'] = cart_obj.id
+            return cart_obj, new_obj
+        
+        
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null = True, blank = True)
     products = models.ManyToManyField(Product, blank = True)

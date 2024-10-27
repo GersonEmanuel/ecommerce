56bffcce10a7a95a62c42a0734cc42c31bf700af
Gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-10-27 17:49:21 -0300

    new view in cart, cart_update

--- carts/views.py
+++ carts/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Cart
+from products.models import Product
 
 # Create your views here.
 def cart_home(request):
     cart_obj, new_obj = Cart.objects.new_or_get(request)
     return render(request, 'carts/home.html', {})
+
+def cart_update(request):
+    product_id = 5
+    product_obj = Product.objects.get(id=product_id)
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    cart_obj.products.add(product_obj)
+    return redirect(product_obj.get_absolute_url())

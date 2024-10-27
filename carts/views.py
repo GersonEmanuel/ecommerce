a3e12c09d322ca25bcce3df37d796b8c2cae4faa
Gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-10-26 22:26:44 -0300

    cart_home

--- carts/views.py
+++ carts/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def cart_home(request):
-    request.session['cart_id'] = 123
-    request.session['user'] = request.user.username
-    return render(request, 'home.html', {})
+    cart_id = request.session.get("cart_id", None)
+    if cart_id is None:
+        print('create new cart')
+        request.session['cart_id'] = 12
+    else:
+        print("Cart ID exists")
+    return render(request, "home.html", {} )

7f7b69ae9116b5894d01069be27b9d5749ca036c
Gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-11-04 18:07:09 -0300

    error in models.py products fixed

--- products/models.py
+++ products/models.py
@@ -40,7 +40,7 @@ class ProductManager(models.Manager):
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length=100)
-    slug = models.SlugField(null=True, blank = True, default = 'normaurl', unique=True)
+    slug = models.SlugField(blank = True, unique=True)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits = 20, default=10)
     image = models.ImageField(upload_to= 'products/', null=True, blank=True)
@@ -59,8 +59,8 @@ class Product(models.Model):
     
     
 def product_pre_save_receiver(sender, instance, *args, **kwargs):
-        if not instance.slug:
-            instance.slug = unique_slug_generator(instance)
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
 
 pre_save.connect(product_pre_save_receiver, sender= Product)
     

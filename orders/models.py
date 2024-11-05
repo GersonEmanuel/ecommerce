d22db9db4a6f2f945ecf4937c14f3ef7a8656b35
Gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-11-05 17:21:03 -0300

    order status choices in orders/ models.pu

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+ORDER_STATUS_CHOICES = (
+    ('created', 'Created'),
+    ('paid', 'Paid'),
+    ('shipped', 'Shipped'),
+    ('refunded', 'Refunded'),
+
+)

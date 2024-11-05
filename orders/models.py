2d21503abe1bf1b4f9cc296e03cea726a8740f7d
Gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-11-05 17:25:00 -0300

    new class order models

--- orders/models.py
+++ orders/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from carts.models import Cart
 
 # Create your models here.
 ORDER_STATUS_CHOICES = (
@@ -6,5 +7,14 @@ ORDER_STATUS_CHOICES = (
     ('paid', 'Paid'),
     ('shipped', 'Shipped'),
     ('refunded', 'Refunded'),
-
 )
+
+class Order(models.Model):
+    order_id = models.CharField(max_length=120, blank=True)
+    # billing_profile = ?
+    # shipping_address = ?
+    # billing_address
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, null = True)
+    status = models.CharField(max_length = 120, default = 'created', choices = ORDER_STATUS_CHOICES )
+    shipping_total = models.DecimalField(default = 5.99, max_digits = 100, decimal_places = 2)
+    # Order total = models.DecimalField(default = 0.00, max_digits = 100, decimal_places = 2)

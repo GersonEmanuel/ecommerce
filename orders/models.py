ff7f4b75676dc6345ad32e70cfb31c1661cba57b
Gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-11-05 17:50:06 -0300

    pre_save fuction in orders/models

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from carts.models import Cart
+from ecommerce.utils import *
+from django.db.models.signals import pre_save
 
 # Create your models here.
 ORDER_STATUS_CHOICES = (
@@ -18,3 +20,9 @@ class Order(models.Model):
     status = models.CharField(max_length = 120, default = 'created', choices = ORDER_STATUS_CHOICES )
     shipping_total = models.DecimalField(default = 5.99, max_digits = 100, decimal_places = 2)
     # Order total = models.DecimalField(default = 0.00, max_digits = 100, decimal_places = 2)
+
+def pre_save_create_order_id(sender, instance *args, **kwargs):
+    if not instance.order_id:
+        instance.order_id = unique_order_id_generator(instance)
+
+pre_save.connect(pre_save_create_order_id, sender = Order)
--- products/models.py
+++ products/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.db.models import Q
-from .utils import unique_slug_generator
+from ecommerce.utils import unique_slug_generator
 from django.db.models.signals import pre_save
 from django.urls import reverse
 

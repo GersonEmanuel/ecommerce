4515389309765bc85b33938b863b57feea3a50b5
gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-11-23 22:58:24 -0300

    total in cart although, i cant access the total of items in the cart i dont know why

--- orders/models.py
+++ orders/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from carts.models import Cart
 from ecommerce.utils import *
-from django.db.models.signals import pre_save
-
+from django.db.models.signals import pre_save, post_save
 # Create your models here.
 ORDER_STATUS_CHOICES = (
     ('created', 'Created'),
@@ -19,10 +18,42 @@ class Order(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, null = True)
     status = models.CharField(max_length = 120, default = 'created', choices = ORDER_STATUS_CHOICES )
     shipping_total = models.DecimalField(default = 5.99, max_digits = 100, decimal_places = 2)
-    # Order total = models.DecimalField(default = 0.00, max_digits = 100, decimal_places = 2)
+    total = models.DecimalField(default = 0.00, max_digits = 100, decimal_places = 2)
+
+    def __str__(self):
+        return self.order_id
+    
+    def update_total(self)->float:
+        cart_total = self.cart.total
+        shipping_total = self.shipping_total
+        new_total = cart_total + shipping_total
+        self.total = new_total
+        self.save()
+        return new_total
 
 def pre_save_create_order_id(sender, instance, *args, **kwargs):
     if not instance.order_id:
         instance.order_id = unique_order_id_generator(instance)
 
 pre_save.connect(pre_save_create_order_id, sender = Order)
+
+
+def post_save_cart_total(sender,instance, created, *args, **kwargs)->None:
+    if not created:
+        cart_obj = instance
+        cart_total = cart_obj.total
+        cart_id = cart_obj.id
+        qs = Order.objects.filter(cart__id=cart_id)
+        if qs.count() == 1:
+            order_obj = qs.first()
+            order_obj.update_total()
+
+post_save.connect(post_save_cart_total, sender = Cart)
+
+def post_save_order(sender, instance, created, *args, **kwargs)->None:
+    if created:
+        instance.update_total()
+
+
+post_save.connect(post_save_order, sender=Order)
+
